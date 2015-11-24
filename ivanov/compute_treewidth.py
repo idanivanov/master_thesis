3250d1789e4f70b30dd848c99b1a9e3285c26707
Ashzigur <sanfan@abv.bg>
2015-11-24 16:39:56 +0100

    Some refinements on computing treewidth; Ignore the data folder

--- ivanov/compute_treewidth.py
+++ ivanov/compute_treewidth.py
@@ -14,21 +14,27 @@ from ivanov.statistics import treewidth
 
 def compute_rballs_tw(in_file):
     graph = rdf.convert_rdf_to_nx_graph(in_file)
+    nodes_in_graph = graph.number_of_nodes()
+    print "Nodes in graph:", nodes_in_graph
     
-    for r in [2, 3, 4, 5]:
-        for d in ["in", "out", "all"]:
-            if d == "all" and r > 3:
+    for r in [2, 3, 4]:
+        for d in ["in"]:
+            if d == "all" and r > 2 or d == "out" and r > 3:
                 break
             print r, d
             out_file = codecs.open("../output/tw_r{0}_{1}".format(r, d), "w", "utf8")
              
             i = 0
             for node in graph.nodes_iter():
+                print "-------------------------------------"
+                print "Node {0}/{1} ({2})".format(i, nodes_in_graph, node)
+                print "r = {0}, d = {1}".format(r, d)
                 rball = algorithms.r_ball(graph, node, r, -1 if d == "in" else 1 if d == "out" else 0)
+                print "r-ball nodes:", rball.number_of_nodes()
                 ap_result = arnborg_proskurowski.get_canonical_representation(rball, False)
+                print "Treewidth: ", ap_result[0]
                 line = u"{0},{1}\n".format(graph.node[node]["labels"][0].replace(u",", u"[comma]"), ap_result[0])
                 out_file.write(line)
-#                 print i, line
 #                 nxext.visualize_graph(rball, node_labels=True, edge_labels=False)
                 i += 1
              
@@ -61,7 +67,7 @@ def aggregate_results(percent=False):
     out_file.close()
 
 if __name__ == '__main__':
-#     compute_rballs_tw("../data/family.rdf.owl.rdf")
-    aggregate_results()
-    aggregate_results(True)
+    compute_rballs_tw("../data/peel.rdf")
+#     aggregate_results()
+#     aggregate_results(True)
     
