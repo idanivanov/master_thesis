ecbd4b38a8d198871ce77c1b7924264796bb9075
Ashzigur <sanfan@abv.bg>
2015-11-24 15:09:20 +0100

    Added automations for treewidth statistics computation

--- ivanov/compute_treewidth.py
+++ ivanov/compute_treewidth.py
@@ -9,23 +9,33 @@ from ivanov.graph import nxext
 from graph import algorithms
 from graph import rdf
 from ivanov.graph.hypergraph import Hypergraph
+import codecs
+from ivanov.statistics import treewidth
 
-if __name__ == '__main__':
-    
-    in_file = "../data/family.rdf.owl.rdf"
+def compute_rballs_tw(in_file):
     graph = rdf.convert_rdf_to_nx_graph(in_file)
     
-    out_file = open("../output/tw_r2_in", "w")
+    for r in [2, 3, 4, 5]:
+        for d in ["in", "out", "all"]:
+            if d == "all" and r > 3:
+                break
+            print r, d
+            out_file = codecs.open("../output/tw_r{0}_{1}".format(r, d), "w", "utf8")
+             
+            i = 0
+            for node in graph.nodes_iter():
+                rball = algorithms.r_ball(graph, node, r, -1 if d == "in" else 1 if d == "out" else 0)
+                ap_result = arnborg_proskurowski.get_canonical_representation(rball, False)
+                line = u"{0},{1}\n".format(graph.node[node]["labels"][0].replace(u",", u"[comma]"), ap_result[0])
+                out_file.write(line)
+#                 print i, line
+#                 nxext.visualize_graph(rball, node_labels=True, edge_labels=False)
+                i += 1
+             
+            out_file.close()
+
+if __name__ == '__main__':
+#     compute_rballs_tw("../data/family.rdf.owl.rdf")
     
-    i = 0
-    for node in graph.nodes_iter():
-        rball = algorithms.r_ball(graph, node, 2, -1)
-        ap_result = arnborg_proskurowski.get_canonical_representation(rball, False)
-        line = "{0},{1}\n".format(graph.node[node]["labels"][0], ap_result[0])
-        out_file.write(line)
-        print i, line
-#         nxext.visualize_graph(rball, node_labels=True, edge_labels=False)
-        i += 1
-    
-    out_file.close()
+    print treewidth.aggregate("../output/tw_r3_all")
     
---
+++ ivanov/statistics/treewidth/__init__.py
@@ -0,0 +1,34 @@
+'''
+Created on Nov 24, 2015
+
+@author: Ivan Ivanov
+'''
+import codecs
+import itertools
+
+'''
+From a file containing treewidths of graphs, get the number of graphs grouped by treewidth.
+
+@param tw_file: CSV file containing treewidths in the following format: each line represents a tuple "graph_id,treewidth"
+@return: Dictionary of the format { treewidth: graphs_count }
+'''
+def aggregate(tw_file_path):
+    lines = read_tw_file(tw_file_path)
+    res = {}
+    for k, g in itertools.groupby(lines, lambda tup: tup[1]):
+        if res.has_key(k):
+            res[k] += sum(1 for _ in g)
+        else:
+            res[k] = sum(1 for _ in g)
+    res["total"] = sum(res.values())
+    return res
+    
+
+def read_tw_file(tw_file_path):
+    tw_file = codecs.open(tw_file_path, "r", "utf8")
+    line = tw_file.readline()
+    while line:
+        items = line[:-1].split(u",")
+        yield (items[0], items[1])
+        line = tw_file.readline()
+    tw_file.close()
