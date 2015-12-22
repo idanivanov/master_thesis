7c9f4835ec52b1ab5147b08fe4f10cc1ebf9a7ab
Ashzigur <sanfan@abv.bg>
2015-12-22 13:42:12 +0100

    Added: r-ball extraction from hypergraphs

--- ivanov/graph/algorithms/__init__.py
+++ ivanov/graph/algorithms/__init__.py
@@ -4,20 +4,18 @@ Created on Nov 21, 2015
 @author: Ivan Ivanov
 '''
 
+from ivanov.graph.hypergraph import Hypergraph
 import networkx as nx
 import copy
 
-'''
-Extract an r-ball around a node in a graph as a subgraph.
-
-@param graph: networkx graph
-@param center: the center node of the r-ball
-@param r: radius of the r-ball (in hops)
-@param edge_dir: the direction of edges to be considered (0 - all edges, 1 - only outgoing, -1 - only incoming)
-
-@return: r-ball as a networkx graph
-'''
 def r_ball(graph, center, r, edge_dir=0):
+    '''Extract an r-ball around a node in a graph as a subgraph.
+    :param graph: Networkx graph
+    :param center: the center node of the r-ball
+    :param r: radius of the r-ball (in hops)
+    :param edge_dir: the direction of edges to be considered (0 - all edges, 1 - only outgoing, -1 - only incoming)
+    :return: r-ball as a Networkx graph
+    '''
     def recurse(u, i):
         if edge_dir <= 0:
             predecessors = graph.predecessors_iter(u)
@@ -40,6 +38,39 @@ def r_ball(graph, center, r, edge_dir=0):
     
     rball = nx.MultiDiGraph()
     rball.add_node(center, attr_dict=copy.deepcopy(graph.node[center]))
+    if r > 0:
+        recurse(center, 1)
+    
+    return rball
+
+def r_ball_hyper(hypergraph, center, r, edge_dir=0):
+    '''The same as r_ball but for Hypergraph.
+    '''
+    assert type(hypergraph) is Hypergraph
+    
+    visited_nodes = set()
+    
+    def recurse(u, i):
+        visited_nodes.add(u)
+        edges = hypergraph.edges_iter(u)
+        for edge in edges:
+            endpoints = hypergraph.endpoints(edge)
+            for v in endpoints:
+                if v not in visited_nodes:
+                    rball.add_node(v, attr_dict=copy.deepcopy(hypergraph.node[v]))
+            edge_attr = hypergraph.edge(edge)
+            direction = edge_attr["direction"]
+            if edge_dir < 0 and any(lambda dir_perm: dir_perm.index(u) > 0, direction) or \
+               edge_dir > 0 and any(lambda dir_perm: dir_perm.index(u) < len(dir_perm) - 1, direction) or \
+               edge_dir == 0:
+                rball.add_edge(hypergraph.endpoints(edge), direction=copy.deepcopy(direction), label=copy.deepcopy(edge_attr["labels"]))
+                if i < r:
+                    for v in endpoints:
+                        if v not in visited_nodes:
+                            recurse(v, i + 1)
+    
+    rball = Hypergraph()
+    rball.add_node(center, attr_dict=copy.deepcopy(hypergraph.node[center]))
     if r > 0:
         recurse(center, 1)
     
