4504ebe74ac8d547c37269c234b446d218517566
Ashzigur <sanfan@abv.bg>
2015-12-30 14:34:32 +0100

    Added: shingle_extraction package

---
+++ ivanov/graph/algorithms/similar_nodes_mining/shingle_extraction/__init__.py
@@ -0,0 +1,18 @@
+'''
+Created on Dec 30, 2015
+
+@author: Ivan Ivanov
+'''
+
+def extract_shingles(feature):
+    '''Extracts (naively) all shingles contained in a feature.
+    :param feature: A Networkx graph.
+    :return A set of shingles.
+    '''
+    pass # TODO: implement
+
+def build_shingle_space(feature):
+    '''Builds a muti-dimensional space where each
+    point corresponds to a possible shingle.
+    '''
+    pass # TODO: implement
