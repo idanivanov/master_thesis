a8548dfd333e86ca06d06cee7270a07f30b0002b
Ashzigur <sanfan@abv.bg>
2016-01-13 11:58:10 +0100

    Estimate number of shingles per feature and print warning for large amounts

--- ivanov/graph/algorithms/similar_nodes_mining/shingle_extraction/__init__.py
+++ ivanov/graph/algorithms/similar_nodes_mining/shingle_extraction/__init__.py
@@ -11,6 +11,10 @@ def extract_shingles(feature):
     :param feature: A Networkx graph.
     :return A generator of shingles.
     '''
+    def estimate_number_of_shingles(nodes_colors):
+        colors = [len(nodes_colors[node]) for node in nodes_colors]
+        return reduce(lambda x, y: x * y, colors)
+    
     def get_all_colorings(nodes_colors):
         '''Get all possible combinations of the feature graph in which
         each node has exactly one color (label).
@@ -34,8 +38,11 @@ def extract_shingles(feature):
         _, shingle = arnborg_proskurowski.get_canonical_representation(shingle_graph)
         return shingle
     
-    # TODO: how to handle exponential growth of shingles?
     nodes_colors = {node: feature.node[node]["labels"] for node in feature.nodes_iter()}
+    shingles_count = estimate_number_of_shingles(nodes_colors)
+    if shingles_count > 100:
+        # TODO: how to handle exponential growth of shingles?
+        print "Warning: too many shingles ({0}) in feature!".format(shingles_count)
     colorings = get_all_colorings(nodes_colors)
     for coloring in colorings:
         yield create_shingle(coloring)
