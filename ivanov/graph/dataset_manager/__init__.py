2a7b96225b89c554a0aeee84141f3f2f0c7ddaef
Ashzigur <sanfan@abv.bg>
2016-02-05 12:07:46 +0100

    Completed read_chemical_compounds

--- ivanov/graph/dataset_manager/__init__.py
+++ ivanov/graph/dataset_manager/__init__.py
@@ -17,29 +17,31 @@ def read_chemical_compounts(in_file):
     chem_graph_database = []
     chem_props = [] # properties of the compounds indexed by their position in the chem_graph_database
     
-    fd = codecs.open(in_file, "r", "utf8")
     current_graph = nx.Graph()
     current_properties = None
-    i = 0
-    for line in fd.readline():
-        if i == 0:
-            assert line.startswith("#")
-            current_properties = line.split(" ")[1:]
-        elif i == 1:
-            nodes = line.split(" ")
-            assert len(nodes) == current_properties[2]
-            for node_id, node_label in enumerate(nodes):
-                current_graph.add_node(node_id, labels=[node_label])
-        else:
-            edges = line.split(" ")
-            edges = list(itertools.groupby(edges, key=lambda x: edges.index(x) - (edges.index(x) % 3)))
-            assert len(edges) == current_properties[3]
-            for _, edge in edges:
-                edge = list(edge)
-                current_graph.add_edge(edge[0], edge[1], label=edge[2])
-            
-            chem_graph_database.append([current_graph])
-            chem_props.append(current_properties)
-        i = (i + 1) % 3
+    with codecs.open(in_file, "r", "utf8") as fp:
+        i = 0
+        for line in fp:
+            if i == 0:
+                if line.startswith("$"): # EOF
+                    break
+                assert line.startswith("#")
+                current_properties = map(lambda x: int(x), line.split(" ")[1:])
+            elif i == 1:
+                nodes = line.split(" ")[:-1]
+                assert len(nodes) == current_properties[2]
+                for node_index, node_label in enumerate(nodes):
+                    current_graph.add_node(node_index + 1, labels=[node_label])
+            else:
+                edges = line.split(" ")[:-1]
+                edges = [edges[e : e + 3] for e in itertools.imap(lambda x: 3 * x, range(len(edges)/3))]
+                assert len(edges) == current_properties[3]
+                for edge in edges:
+                    edge = list(edge)
+                    current_graph.add_edge(int(edge[0]), int(edge[1]), label=edge[2])
+                
+                chem_graph_database.append([current_graph])
+                chem_props.append(current_properties)
+            i = (i + 1) % 3
     
     return chem_graph_database, chem_props
