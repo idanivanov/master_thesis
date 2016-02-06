de4f133ce5b95725391e37e2be74e9c393ef0a9f
Ashzigur <sanfan@abv.bg>
2016-02-06 11:40:01 +0100

    Updated crossval_similar_chem_graphs

--- ivanov/crossval_similar_chem_graphs.py
+++ ivanov/crossval_similar_chem_graphs.py
@@ -3,12 +3,20 @@ Created on Feb 4, 2016
 
 @author: Ivan Ivanov
 '''
+from ivanov.graph.algorithms.similar_graphs_mining import crossval
 from ivanov.graph import dataset_manager
 from ivanov import helpers
 
-path = "data/"
+dataset = "mutagenicity"
+wl_iter_range = range(5)
+k = 25
+L = 265
+output_dir = "../output_chem/crossval_test/"
 
 if __name__ == '__main__':
-    in_file = helpers.datasets["mutagenicity"]["files"][0]
+    in_file = helpers.datasets[dataset]["files"][0]
     graph_database, chem_props = dataset_manager.read_chemical_compounts(in_file)
-    print chem_props
+    cols_count = len(graph_database)
+    target_values = map(lambda x: x[1], chem_props)
+    best_model = crossval.loo_crossval(graph_database, cols_count, target_values, wl_iter_range, k, L, output_dir)
+    print "Best model:", best_model
