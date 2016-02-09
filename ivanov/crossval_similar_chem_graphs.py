1414001cabe55438d87e9819821084fea9c88efb
Ashzigur <sanfan@abv.bg>
2016-02-09 17:01:18 +0100

    crossval_similar_chem_graphs new setting

--- ivanov/crossval_similar_chem_graphs.py
+++ ivanov/crossval_similar_chem_graphs.py
@@ -8,7 +8,7 @@ from ivanov.graph import dataset_manager
 from ivanov import helpers
 
 dataset = "mutagenicity"
-wl_iter_range = range(5)
+wl_iter_range = range(0, 100)
 k_L_range = [
     (25, 14),   # inflation point 0.1
     (25, 58),   # inflation point 0.15
@@ -16,7 +16,18 @@ k_L_range = [
     (15, 211),  # inflation point 0.3
     (10, 165),  # inflation point 0.4
     (5, 32)     # inflation point 0.5
+#     (15, 75),   # inflation point 0.25
+#     (15, 92),   # inflation point 0.26
+#     (15, 112),   # inflation point 0.27
+#     (15, 138),   # inflation point 0.28
+#     (15, 170),   # inflation point 0.29
+#     (15, 261),   # inflation point 0.31
+#     (14, 221),   # inflation point 0.32
+#     (14, 272),   # inflation point 0.33
+#     (13, 222),   # inflation point 0.34
+#     (13, 270),   # inflation point 0.35
 ]
+p_range = [4]
 output_dir = "../output_chem/crossval_test/"
 
 if __name__ == '__main__':
@@ -24,5 +35,6 @@ if __name__ == '__main__':
     graph_database, chem_props = dataset_manager.read_chemical_compounts(in_file)
     cols_count = len(graph_database)
     target_values = map(lambda x: x[1], chem_props)
-    best_model = crossval.loo_crossval(graph_database, cols_count, target_values, wl_iter_range, k_L_range, output_dir)
+#     best_model = crossval.loo_crossval(graph_database, cols_count, target_values, wl_iter_range, k_L_range, output_dir)
+    best_model = crossval.loo_crossval_pnn(graph_database, cols_count, target_values, wl_iter_range, p_range, output_dir)
     print "Best model:", best_model
