eb3bc882fdddf0410222b564788aab16c4b96e8f
Ashzigur <sanfan@abv.bg>
2016-02-12 16:01:07 +0100

    new crossval for rdf setting

--- ivanov/crossval_similar_rdf_nodes.py
+++ ivanov/crossval_similar_rdf_nodes.py
@@ -8,25 +8,22 @@ from ivanov.graph.hypergraph import Hypergraph
 from ivanov.graph import rdf
 from ivanov import helpers
 
-dataset = "tele-ext"
-# wl_iter_range = range(0, 5)
-wl_iter_range = range(2, 3)
+dataset = "drugadmin"
+wl_iter_range = range(0, 10)
 k_L_range = [
-#     (20, 1),    # inflection point ~0.
-#     (15, 5),    # inflection point 0.1
+    (20, 1),    # inflection point ~0.
+    (15, 5),    # inflection point 0.1
     (10, 9),    # inflection point 0.2
-#     (7, 12),    # inflection point 0.3
-#     (5, 13),    # inflection point 0.4
-#     (4, 16),    # inflection point 0.5
-#     (3, 16),    # inflection point 0.6
-#     (2, 11),    # inflection point 0.7
-#     (2, 25),    # inflection point 0.8
-#     (1, 10),    # inflection point 0.9
-#     (1, 20),    # inflection point ~1.
+    (7, 12),    # inflection point 0.3
+    (5, 13),    # inflection point 0.4
+    (4, 16),    # inflection point 0.5
+    (3, 16),    # inflection point 0.6
+    (2, 11),    # inflection point 0.7
+    (2, 25),    # inflection point 0.8
+    (1, 10),    # inflection point 0.9
+    (1, 20),    # inflection point ~1.
 ]
 p_range = [4]
-# r_in_range = range(1, 4)
-# r_out_range = range(1, 4)
 r_in_range = range(3, 4)
 r_out_range = range(2, 3)
 r_all_range = [0]
