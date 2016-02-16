ea1bc46d74af6900ce2d1d5672363a35c0aa1b5e
Ashzigur <sanfan@abv.bg>
2016-02-16 11:38:44 +0100

    crossval: remove colors from query node that was left out during crossval

--- ivanov/graph/algorithms/similar_graphs_mining/crossval/__init__.py
+++ ivanov/graph/algorithms/similar_graphs_mining/crossval/__init__.py
@@ -44,9 +44,9 @@ def predict_target_majority(similar_targets):
         else:
             return 0
 
-def loo_crossval_sketch(graph_database, cols_count, target_values, wl_iter_range, k_L_range, output_dir, base_model={}):
+def loo_crossval_sketch(graph_database, cols_count, target_values, wl_iter_range, k_L_range, output_dir, base_model={}, prepare_query_function=None):
     '''Leave-one-out cross-validation.
-    :param graph_database: Defined the same way as for CHaracteristicMatrix constructor.
+    :param graph_database: Defined the same way as for CharacteristicMatrix constructor.
     :param cols_count: Number of elements in the database.
     :param target_values: A list of the size of the graph_database, where each element
     indicates the real target value of the corresponding (by index) element in the database.
@@ -56,12 +56,18 @@ def loo_crossval_sketch(graph_database, cols_count, target_values, wl_iter_range
 #     representing a single entity in the database and sketch_matrix is a sketch matrix. The function
 #     should return a real value. The cross-validation will find the model that maximizes this function.
     :param output_dir: A local directory, that will be used to save the sketch matrices of all models.
+    :param base_model: A base model that is going to be extended by the new parameters.
+    :param prepare_query_function: A function to prepare the query record that is left out by the cross-validation.
     :return The best model as a dictionary in the form: {quality, wl_iterations}.
     '''
-    def quality(i, sketch_matrix):
-        col_i = sketch_matrix.get_column(i)
+    def quality(i, ch_matrix, sketch_matrix):
+        if prepare_query_function:
+            col_i = prepare_query_function(i, graph_database, ch_matrix, sketch_matrix)
+        else:
+            col_i = sketch_matrix.get_column(i)
         similar_cols = list(sketch_matrix.get_similar_columns(col_i))
-        similar_cols.remove(i)
+        if i in similar_cols:
+            similar_cols.remove(i)
         similar_targets = map(lambda c: target_values[c], similar_cols)
         true_target_i = target_values[i]
         estimated_target_i = predict_target_majority(similar_targets)
@@ -69,12 +75,12 @@ def loo_crossval_sketch(graph_database, cols_count, target_values, wl_iter_range
 #         print "Similar cols:", similar_cols
 #         print "Similar targets:", similar_targets
 #         print "--------------------------------------"
-        fp = open(output_dir + "classification", "a")
-        fp.write("Col: {0}, Target: {1}, Est. target: {2}\n".format(i, true_target_i, estimated_target_i))
-        fp.write("Similar cols: {0}\n".format(similar_cols))
-        fp.write("Similar targets: {0}\n".format(similar_targets))
-        fp.write("--------------------------------------\n")
-        fp.close()
+#         fp = open(output_dir + "classification", "a")
+#         fp.write("Col: {0}, Target: {1}, Est. target: {2}\n".format(i, true_target_i, estimated_target_i))
+#         fp.write("Similar cols: {0}\n".format(similar_cols))
+#         fp.write("Similar targets: {0}\n".format(similar_targets))
+#         fp.write("--------------------------------------\n")
+#         fp.close()
         if type(true_target_i) is list:
             return int(estimated_target_i in true_target_i) # zero-one loss
         else:
@@ -85,20 +91,20 @@ def loo_crossval_sketch(graph_database, cols_count, target_values, wl_iter_range
     models_file = open(output_dir + "models", "a")
     
     for wl_iterations in wl_iter_range:
-        start = time.time()
-        ch_matrix = CharacteristicMatrix(graph_database, cols_count, wl_iterations=wl_iterations, print_progress=True)
-        print "Building characteristic matrix for wl_iter =", wl_iterations, "took:", time.time() - start
+#         start = time.time()
+        ch_matrix = CharacteristicMatrix(graph_database, cols_count, wl_iterations=wl_iterations, print_progress=False)
+#         print "Building characteristic matrix for wl_iter =", wl_iterations, "took:", time.time() - start
         for k, L in k_L_range:
-            start = time.time()
+#             start = time.time()
             sketch_matrix = SketchMatrix(k, L, ch_matrix)
-            print "Building sketch matrix for k={0} and L={1} took:".format(k, L), time.time() - start
+#             print "Building sketch matrix for k={0} and L={1} took:".format(k, L), time.time() - start
 #             sketch_matrix.save_to_file(output_dir + "sketch_matrix_wl{0}_k{1}_L{2}".format(wl_iterations, k, L))
-            start = time.time()
+#             start = time.time()
             avg_quality = 0.
             for i in range(cols_count):
-                avg_quality += float(quality(i, sketch_matrix))
+                avg_quality += float(quality(i, ch_matrix, sketch_matrix))
             avg_quality /= cols_count
-            print "Classification took:", time.time() - start
+#             print "Classification took:", time.time() - start
             current_model = model_infl_point(avg_quality, wl_iterations, k, L, base_model=base_model)
             print current_model
             models_file.write(str(current_model) + ",\n")
@@ -162,6 +168,12 @@ def loo_crossval_threshold(graph_database, cols_count, target_values, wl_iter_ra
 #         print "Similar cols:", similar_cols
 #         print "Similar targets:", similar_targets
 #         print "--------------------------------------"
+#         fp = open(output_dir + "classification", "a")
+#         fp.write("Col: {0}, Target: {1}, Est. target: {2}\n".format(i, true_target_i, estimated_target_i))
+#         fp.write("Similar cols: {0}\n".format(similar_cols))
+#         fp.write("Similar targets: {0}\n".format(similar_targets))
+#         fp.write("--------------------------------------\n")
+#         fp.close()
         if type(true_target_i) is list:
             return int(estimated_target_i in true_target_i) # zero-one loss
         else:
@@ -169,7 +181,7 @@ def loo_crossval_threshold(graph_database, cols_count, target_values, wl_iter_ra
     
     return loo_crossval_naive(graph_database, cols_count, target_values, wl_iter_range, infl_point_range, quality, output_dir, base_model)
 
-def loo_crossval_pnn(graph_database, cols_count, target_values, wl_iter_range, p_range, output_dir):
+def loo_crossval_pnn(graph_database, cols_count, target_values, wl_iter_range, p_range, output_dir, base_model={}):
     '''Similar to loo_crossval_sketch but computes directly the Jaccard
     similarities between the columns in the characteristic matrix,
     without using a sketch matrix. Not applicable for big datasets.
--- ivanov/graph/algorithms/similar_nodes_mining/crossval/__init__.py
+++ ivanov/graph/algorithms/similar_nodes_mining/crossval/__init__.py
@@ -11,6 +11,16 @@ def model(r_in, r_out, r_all):
     return {"r_in": r_in, "r_out": r_out, "r_all": r_all}
 
 def loo_crossval(hypergraph, wl_iter_range, r_in_range, r_out_range, r_all_range, output_dir, k_L_range=None, infl_point_range=None, p_range=None):
+    def prepare_query_sketch_column(record_index, graph_database, ch_matrix, sketch_matrix):
+        query_node_id, record_graphs = graph_database[record_index]
+        record_graphs = [graph.copy() for graph in record_graphs]
+        for graph in record_graphs:
+            query_node = graph.node[query_node_id]
+            query_node["labels"] = ["0"] # set color of query node to unknown
+        
+        fingerprints = ch_matrix.compute_column_fingerprints(record_graphs)
+        return sketch_matrix.compute_column(fingerprints)
+    
     best_model = sgm_crossval.model(-1, -1, base_model=model(-1, -1, -1))
     
     for r_in in r_in_range:
@@ -22,7 +32,7 @@ def loo_crossval(hypergraph, wl_iter_range, r_in_range, r_out_range, r_all_range
                 cols_count = hypergraph.number_of_nodes()
                 target_values = map(lambda n: hypergraph.node[n]["labels"], hypergraph.nodes_iter())
                 if k_L_range:
-                    current_model = sgm_crossval.loo_crossval_sketch(rballs_database, cols_count, target_values, wl_iter_range, k_L_range, output_dir, base_model=base_model)
+                    current_model = sgm_crossval.loo_crossval_sketch(rballs_database, cols_count, target_values, wl_iter_range, k_L_range, output_dir, base_model=base_model, prepare_query_function=prepare_query_sketch_column)
                 elif infl_point_range:
                     current_model = sgm_crossval.loo_crossval_threshold(rballs_database, cols_count, target_values, wl_iter_range, infl_point_range, output_dir, base_model=base_model)
                 else:
