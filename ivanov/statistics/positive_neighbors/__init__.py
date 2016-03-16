7c25e0e9f71714810358a52efce3c71569cd491b
Ashzigur <sanfan@abv.bg>
2016-03-16 18:13:35 +0100

    positive_neighbors: make it work with radius-based neighbor search

--- ivanov/statistics/positive_neighbors/__init__.py
+++ ivanov/statistics/positive_neighbors/__init__.py
@@ -10,16 +10,45 @@ from ivanov import helpers
 import numpy as np
 import itertools
 
-def get_positive_neighbors_counts(X, y, k, r=None, positive_label=1):
-    nn = neighbors.NearestNeighbors(n_neighbors=k + 1, algorithm="auto").fit(X)
+def get_positive_neighbors_counts(X, y, k=None, radius=None, positive_label=1):
+    assert bool(k) ^ bool(radius)
+    
+    if k:
+        assert type(k) is int
+        nn = neighbors.NearestNeighbors(n_neighbors=k + 1, algorithm="auto").fit(X)
+    else:
+        assert type(radius) is float
+        nn = neighbors.NearestNeighbors(radius=radius, algorithm="auto").fit(X)
     pos_X_indices = np.where(y == positive_label)[0]
     positive_X = X[pos_X_indices]
-    neigh = nn.kneighbors(positive_X, return_distance=False)
+    if k:
+        neigh = nn.kneighbors(positive_X, return_distance=False)
+    else:
+        neigh = nn.radius_neighbors(positive_X, return_distance=False)
     neigh = neigh[:, 1:] # remove self from neighbors
     neigh_targets = np.vectorize(lambda x: y[x])(neigh)
     pos_neigh_counts = np.sum(neigh_targets == positive_label, axis=1)
     avg_pos_neigh_count = np.average(pos_neigh_counts)
     return itertools.izip(pos_X_indices, pos_neigh_counts), avg_pos_neigh_count
+
+def arrange_results(models):
+    models.sort(key=lambda x: (x["wl_iterations"], x["k"]))
+    max_k = max(map(lambda x: x["k"], models))
+    filler = " & "
+    arranged_str = ""
+    new_line = True
+    for m in models:
+        if new_line:
+            arranged_str += "\\num{{{0}}}{1}".format(m["wl_iterations"], filler)
+            new_line = False
+        arranged_str += "\\num{{{0}}}".format(m["avg_count_prop"])
+        if m["k"] == max_k:
+            arranged_str += "\\\\\n"
+            new_line = True
+        else:
+            arranged_str += filler
+    
+    return arranged_str
 
 if __name__ == '__main__':
     
@@ -42,6 +71,7 @@ if __name__ == '__main__':
             X, y = dataset_manager.read_svm_light_bool_data_to_sparse(data_file)
 #             y = np.vectorize(lambda t: 1 if t == 2 else -1)(y) # Only for A_vs_M
             y = np.vectorize(lambda t: 1 if t == 2 else t)(y) # Only for AM_vs_I and A_vs_I
+#             for k in [1, 5, 10, 50, 100, 500, 1000, 1500]:
             for k in [1, 5, 10, 50, 100, 500, 1000, 5000, 10000]:
                 counts, avg_count = get_positive_neighbors_counts(X, y, k)
                 model = {"wl_iterations": w, "k": k, "avg_count": avg_count, "avg_count_prop": avg_count / k}
