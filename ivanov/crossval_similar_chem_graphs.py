3bedd0e89bb0b5e72fcedaead7b9c7ae6c565810
Ashzigur <sanfan@abv.bg>
2016-03-23 14:23:23 +0100

    crossval_similar_chem_graphs updated

--- ivanov/crossval_similar_chem_graphs.py
+++ ivanov/crossval_similar_chem_graphs.py
@@ -26,17 +26,17 @@ k_L_range = [
 ]
 p_range = range(30)
 infl_point_range = [0., 0.0000001, 0.1, 0.15, 0.2, 0.25, 0.26, 0.27, 0.28, 0.29, 0.3, 0.31, 0.32, 0.33, 0.34, 0.35, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.]
-window_size_range = range(1, 20)
+window_size_range = [6] # range(1, 20)
 output_dir = "../output_chem/"
 
-def crossval_small_dataset(w_shingles):
+def crossval_small_dataset(shingles_type):
     in_file = helpers.datasets[dataset]["files"][0]
     graph_database = list(dataset_manager.read_chemical_compounts(in_file))
     for window_size in window_size_range:
         base_model = {"window_size": window_size}
-#         best_model = crossval.loo_crossval_sketch(graph_database, wl_iter_range, k_L_range, output_dir, cols_count=188, base_model=base_model, w_shingles=w_shingles, window_size=window_size)
-        best_model = crossval.loo_crossval_pnn(graph_database, wl_iter_range, p_range, output_dir, base_model=base_model, w_shingles=w_shingles, window_size=window_size)
-#         best_model = crossval.loo_crossval_threshold(graph_database, wl_iter_range, infl_point_range, output_dir, base_model=base_model, w_shingles=w_shingles, window_size=window_size)
+#         best_model = crossval.loo_crossval_sketch(graph_database, wl_iter_range, k_L_range, output_dir, cols_count=188, base_model=base_model, shingles_type=shingles_type, window_size=window_size)
+        best_model = crossval.loo_crossval_pnn(graph_database, wl_iter_range, p_range, output_dir, base_model=base_model, shingles_type=shingles_type, window_size=window_size)
+#         best_model = crossval.loo_crossval_threshold(graph_database, wl_iter_range, infl_point_range, output_dir, base_model=base_model, shingles_type=shingles_type, window_size=window_size)
         print "Best model:", best_model
 
 def crossval_big_dataset():
@@ -51,17 +51,17 @@ def crossval_big_dataset():
         best_model = crossval.d_fold_crossval(data, examples_count, folds_count, k_L_range, output_dir, base_model=base_model)
         print "Best model:", best_model
 
-def crossval_small_rdf_dataset(w_shingles):
+def crossval_small_rdf_dataset(shingles_type):
     in_files = helpers.datasets[dataset]["files"]
     compounds_targets_file = helpers.datasets[dataset]["compounds_targets"]["sample-1"]
     uri_prefix = "http://dl-learner.org/mutagenesis#"
     graph_database = list(dataset_manager.prepare_rdf_chemical_data(in_files, compounds_targets_file, uri_prefix))
-#     best_model = crossval.loo_crossval_sketch(graph_database, wl_iter_range, k_L_range, output_dir, cols_count=188, w_shingles=w_shingles)
-#     best_model = crossval.loo_crossval_pnn(graph_database, wl_iter_range, p_range, output_dir, w_shingles=w_shingles)
-    best_model = crossval.loo_crossval_threshold(graph_database, wl_iter_range, infl_point_range, output_dir, w_shingles=w_shingles)
+#     best_model = crossval.loo_crossval_sketch(graph_database, wl_iter_range, k_L_range, output_dir, cols_count=188, shingles_type=shingles_type)
+#     best_model = crossval.loo_crossval_pnn(graph_database, wl_iter_range, p_range, output_dir, shingles_type=shingles_type)
+    best_model = crossval.loo_crossval_threshold(graph_database, wl_iter_range, infl_point_range, output_dir, shingles_type=shingles_type)
     print "Best model:", best_model
 
 if __name__ == '__main__':
-    crossval_small_dataset(True)
+    crossval_small_dataset("all")
 #     crossval_big_dataset()
-#     crossval_small_rdf_dataset(True)
+#     crossval_small_rdf_dataset("all")
