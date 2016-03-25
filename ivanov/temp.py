72f0d83cf343face6eafe29e515b89941a2b940a
Ashzigur <sanfan@abv.bg>
2016-03-25 16:56:43 +0100

    temporary file

--- ivanov/temp.py
+++ ivanov/temp.py
@@ -17,12 +17,14 @@ if __name__ == '__main__':
 
 #     dataset = "mutagenicity"
 #     in_files = helpers.datasets[dataset]["files"]
+# #     output_dir = "/media/ivan/204C66C84C669874/Uni-Bonn/Thesis/Main/5_Complete_Project/Workspace/master_thesis/output_chem/mutagenicity/data/"
+# #     dataset_manager.build_svmlight_chemical_data(in_files, 11, output_dir)
 #     output_dir = "/media/ivan/204C66C84C669874/Uni-Bonn/Thesis/Main/5_Complete_Project/Workspace/master_thesis/output_chem/mutagenicity/data_w_{0}/"
-#     for w in range(1, 20):
+#     for w in [5]: #range(1, 20):
 #         dataset_manager.build_svmlight_chemical_data(in_files, 5, output_dir.format(w), shingles_type="w-shingles", window_size=w)
     
     dataset = "nci-hiv"
     in_files = helpers.datasets[dataset]["files"]
     output_dir = "/media/ivan/204C66C84C669874/Uni-Bonn/Thesis/Main/5_Complete_Project/Workspace/master_thesis/output_chem/nci_hiv/data_w_{0}/"
     for w in [5, 10, 20]: # range(5, 6):
-        dataset_manager.build_svmlight_chemical_data(in_files, 3, output_dir.format(w), shingles_type="w-shingles", window_size=w)
+        dataset_manager.build_svmlight_chemical_data(in_files, 3, output_dir.format(w), shingles_type="w-shingles", window_size=w, fingerprints=True)
