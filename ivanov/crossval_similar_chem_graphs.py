0e50064b83fc9ab2832a4357c3e6365ae32e19e9
Ashzigur <sanfan@abv.bg>
2016-04-21 10:09:44 +0200

    Small fix

--- ivanov/crossval_similar_chem_graphs.py
+++ ivanov/crossval_similar_chem_graphs.py
@@ -9,7 +9,7 @@ from ivanov import helpers
 from itertools import imap
 
 dataset = "mutagenicity"
-wl_iter_range = range(0, 12)
+wl_iter_range = range(5, 6)
 k_L_range = [
     (20, 1),    # inflection point ~0.
     (15, 5),    # inflection point 0.1
@@ -26,28 +26,29 @@ k_L_range = [
 ]
 p_range = range(30)
 infl_point_range = [0., 0.0000001, 0.1, 0.15, 0.2, 0.25, 0.26, 0.27, 0.28, 0.29, 0.3, 0.31, 0.32, 0.33, 0.34, 0.35, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.]
-window_size_range = [6] # range(1, 20)
+window_size_range = [5] # range(1, 20)
 output_dir = "../output_chem/"
 
 def crossval_small_dataset(shingles_type):
     in_file = helpers.datasets[dataset]["files"][0]
     graph_database = list(dataset_manager.read_chemical_compounts(in_file))
-#     for window_size in window_size_range:
-#         base_model = {"window_size": window_size}
-#         best_model = crossval.loo_crossval_sketch(graph_database, wl_iter_range, k_L_range, output_dir, cols_count=188, base_model=base_model, shingles_type=shingles_type, window_size=window_size)
-#         best_model = crossval.loo_crossval_pnn(graph_database, wl_iter_range, p_range, output_dir, base_model=base_model, shingles_type=shingles_type, window_size=window_size)
-#         best_model = crossval.loo_crossval_threshold(graph_database, wl_iter_range, infl_point_range, output_dir, base_model=base_model, shingles_type=shingles_type, window_size=window_size)
-    
-    # TODO: drop-edges method is just for test
-    for drop_proba in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
-        new_graph_database = []
-        for record in graph_database:           
-            reduced_graph = algorithms.drop_edges_by_probability(record[1][0], drop_proba)
-            new_record = (record[0], [record[1][0], reduced_graph], record[2])
-            new_graph_database.append(new_record)
-        base_model = {"drop_proba": drop_proba}
-        best_model = crossval.loo_crossval_pnn(graph_database, wl_iter_range, p_range, output_dir, base_model=base_model)
+    for window_size in window_size_range:
+        base_model = {"window_size": window_size}
+#         best_model = crossval.loo_crossval_sketch(graph_database, wl_iter_range, k_L_range, output_dir, cols_count=188, base_model=base_model, shingles_type=shingles_type, window_size=window_size, accumulate_wl_shingles=False)
+        best_model = crossval.loo_crossval_pnn(graph_database, wl_iter_range, p_range, output_dir, base_model=base_model, shingles_type=shingles_type, window_size=window_size, accumulate_wl_shingles=False)
+#         best_model = crossval.loo_crossval_threshold(graph_database, wl_iter_range, infl_point_range, output_dir, base_model=base_model, shingles_type=shingles_type, window_size=window_size, accumulate_wl_shingles=False)
         print "Best model:", best_model
+    
+#     # TODO: drop-edges method is just for test
+#     for drop_proba in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+#         new_graph_database = []
+#         for record in graph_database:           
+#             reduced_graph = algorithms.drop_edges_by_probability(record[1][0], drop_proba)
+#             new_record = (record[0], [record[1][0], reduced_graph], record[2])
+#             new_graph_database.append(new_record)
+#         base_model = {"drop_proba": drop_proba}
+#         best_model = crossval.loo_crossval_pnn(graph_database, wl_iter_range, p_range, output_dir, base_model=base_model)
+#         print "Best model:", best_model
 
 def crossval_big_dataset():
     path_to_data = "/media/ivan/204C66C84C669874/Uni-Bonn/Thesis/Main/6_Results/svm/nci_hiv/data/A_vs_M/"
